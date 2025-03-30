06531186d093a83f8417d2e54f5f1abb5881e872
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-03-30 15:37:16 +0000

    //Inserindo os operadores de atribuição

---
+++ operadoresdeatribuicao.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    atribuição Simples(+)
+    Atribuição com Soma (+=)
+    Atribuição com Subtração (-=)
+    Atribuição com multiplicação (*=)
+    Atribuição com Divisão (/=)
+    */
+
+    //Entrando com as variáveis
+    int numero1, numero2, resultado;
+
+    resultado = 10;
+    printf("Resultado: %d\n", resultado); //É como sairá o resultado na tela
+
+    //usando os operadores de atribuição
+
+    resultado += 20;
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+
+
+
+}
+
+
+
+
+
+    
+    
