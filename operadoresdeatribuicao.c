2307409ff2f2816c6b888b969028410c9902ae55
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-03-30 16:11:07 +0000

    Código concluído

--- operadoresdeatribuicao.c
+++ operadoresdeatribuicao.c
@@ -10,15 +10,29 @@ int main(){
     */
 
     //Entrando com as variáveis
-    int numero1, numero2, resultado;
+    int numero1 = 10, numero2, resultado;
 
     resultado = 10;
     printf("Resultado: %d\n", resultado); //É como sairá o resultado na tela
 
-    //usando os operadores de atribuição
+     //usando os operadores de atribuição
 
+    //resultado = resultado + 20
     resultado += 20;
-    printf("Resultado: %d\n", resultado);
+    printf("Resultado: %d\n", resultado); //faz a soma e depois atribuirá o valor
+
+     //resultado = resultado - numero1
+    resultado -= numero1;
+    printf("Resultado: %d\n", resultado); //faz a subtração e depois atribuirá o valor
+    
+     //resultado - resultado *5
+    resultado *= 5;
+    printf("Resultado: %d\n", resultado); 
+
+     //resultado = resultado /2
+    resultado /= 2;
+    printf("Resultado: %d\n", resultado); 
+
 
     return 0;
 
