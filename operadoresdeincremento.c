d42aac016f838a991a8c3f026e4c43cb214ac28b
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-03-30 16:35:00 +0000

    Antes e após realizar o incremento

---
+++ operadoresdeincremento.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    Pré-Incremento ++a a--
+    Pós-Incremento a++
+    Decremento(--)
+    Pré-Decremento --a
+    Pós-Decremento a--
+    */
+
+    int numero1 = 1;
+
+    printf("Antes incremento: %d \n", numero1); //antes de realizar o incremento
+    numero1++;
+    printf("Após incremento: %d \n", numero1); //após realizar o incremento
+
+    return 0;
+
+    
+  
+}
+    
+
