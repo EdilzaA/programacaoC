395552317c80d8484fd3a0dabf0e539f23d3e072
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-03-30 21:02:58 +0000

    modified:   codar.c

--- codar.c
+++ codar.c
@@ -2,9 +2,21 @@
 
 int main(){
     
-    
+int nota1, nota2, nota3;
+int media;
 
+printf(“Programa de Cálculo de Média\n”);
+printf(“Digite a sua primeira nota: \n”);
+scanf(“%d”, &nota1);
 
-    return 0;
+printf(“Digite a sua segunda nota: \n”);
+scanf(“%d”, &nota2);
+
+printf(“Digite a sua terceita nota: \n”);
+scanf(“%d”, &nota3);
+
+media = (nota1 + nota2 + nota3) / 3;
+
+return 0;
 
 }
