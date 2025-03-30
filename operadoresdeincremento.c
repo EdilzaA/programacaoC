d46cf8a061282874c9e8975cce9d54a8fcef183e
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-03-30 19:08:45 +0000

    modified:   operadoresdeincremento.c

--- operadoresdeincremento.c
+++ operadoresdeincremento.c
@@ -9,12 +9,40 @@ int main(){
     Pós-Decremento a--
     */
 
-    int numero1 = 1;
+    int numero1 = 1, resultado;
 
     printf("Antes incremento: %d \n", numero1); //antes de realizar o incremento
-    numero1++;
-    printf("Após incremento: %d \n", numero1); //após realizar o incremento
+    // numero1 = numero1 +1;
+    //numero1 += 1;
+    //Pós incremento:
+     //resultado - numero1; - primeito etribui
+     //numero1++; depois incrementei
 
+     resultado = numero1++; //o operador fica antes do número
+     //printf("Após o pre-incremento: %d\n", numero1);
+     printf("Após Pós-incremento - Numero 1: %d  - Resultado: %d\n", numero1, resultado); //após realizar o incremento
+     numero1++; 
+
+     resultado = ++numero1; //o operador fica antes do número
+    //printf("Após o pre-incremento: %d\n", numero1);
+    //após realizar o incremento
+    numero1++;
+
+     //numero1 = numero1 - 1;
+     //numero1-= 1;
+     //numero1--;
+     //printf('Apos decremento: %d\n", numero1)
+
+     resultado = numero1--;
+     printf("Após Pos-decremento - Numero 1: %d  - Resultado: %d\n", numero1, resultado);
+
+     resultado = --numero1;
+     printf("Após Pré-decremento - Numero 1: %d  - Resultado: %d\n", numero1, resultado);
+
+
+
+ 
+    
     return 0;
 
     
