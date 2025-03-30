0879c3650449b5fc26e8e05e3a26a691507255c2
EdilzaA <edilzaprogramacao.github@gmail.com>
2025-03-30 20:05:35 +0000

    modified:   conversao_implicita.c

--- conversao_implicita.c
+++ conversao_implicita.c
@@ -3,7 +3,7 @@
 int main(){
     int a = 10;
     float b = 3.5;
-    float resultado = a + b; //a é convetido implicitamente para float
+    float resultado = a + b; //a é convetido implicitamente para float, 10+3.5, variáveis inteira e de ponto flutuante
 
     printf("Resultado: %.2f\n", resultado);
     
